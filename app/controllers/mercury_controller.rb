fa0f7b36f52afaf7b8649c1b08c6a44caa851d55
Will Bridges <will@fashionbytesinc.com>
2017-05-18 12:41:28 -0500

    Making controller work with Rails5

--- app/controllers/mercury_controller.rb
+++ app/controllers/mercury_controller.rb
@@ -2,28 +2,28 @@ class MercuryController < ActionController::Base
   include ::Mercury::Authentication
 
   protect_from_forgery
-  before_action :authenticate, :only => :edit
+  before_action :authenticate, only: :edit
   layout false
 
   def edit
-    render :text => '', :layout => 'mercury'
+    render html: '', layout: 'mercury'
   end
 
   def resource
-    render :action => "/#{params[:type]}/#{params[:resource]}"
+    render action: "/#{params[:type]}/#{params[:resource]}"
   end
 
   def snippet_options
     @options = params[:options] || {}
-    render :action => "/snippets/#{params[:name]}/options"
+    render action: "/snippets/#{params[:name]}/options"
   end
 
   def snippet_preview
-    render :action => "/snippets/#{params[:name]}/preview"
+    render action: "/snippets/#{params[:name]}/preview"
   end
 
   def test_page
-    render :text => params
+    render plain: params
   end
 
   private
